5d3a92793f0a0938359731f8f98ed7ebedaa9475
SAUD ALNASSER <saud4services@gmail.com>
2024-11-17 21:13:41 +0300

    feat: create basic slime enemy

---
+++ scripts/slime.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var speed := 60.0
+var direction := 1
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var raycast_right: RayCast2D = $Sight/RayCast2DRight
+@onready var raycast_left: RayCast2D = $Sight/RayCast2DLeft
+
+func _process(delta: float) -> void:
+	if raycast_right.is_colliding():
+		direction = -1
+		sprite.flip_h = true
+
+	if raycast_left.is_colliding():
+		direction = 1
+		sprite.flip_h = false
+
+	position.x += direction * speed * delta
