8b2aaecf03cd0434feaaa4d1ed0a7bc6e3d53c6b
SAUD ALNASSER <saud4services@gmail.com>
2024-11-19 03:31:54 +0300

    feat: implement player moving animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
@@ -14,9 +16,27 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+  # Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
+
+
+	# Flip the Sprite
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
+
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			sprite.play("idle")
+		else:
+			sprite.play("run")
+	else:
+		sprite.play("jump")
+
+
+  # Apply the movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
