e65dfc6d23362f62a20bffaf6d165cf18799c1ab
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-11-22 06:29:46 +0300

    feat: play sound on player jump (#15)
    ### TL;DR
    Added jump sound effect to player character
    
    ### What changed?
    - Added a new audio stream player to the player scene
    - Connected jump sound effect asset
    - Configured audio settings with -5.0 volume on SFX bus
    - Added logic to play jump sound when player performs a jump action while on the floor

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -17,6 +18,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		if is_on_floor():
+			jump_sound.play()
 
   # Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
